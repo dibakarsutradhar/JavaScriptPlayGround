b8f31163c5e45eb561b3a47407346d47c861d102
Dibakar <crossbonesdipu@gmail.com>
2018-03-01 04:14:34 +0800

    Precedence

--- Section 01.3 Operators/script.js
+++ Section 01.3 Operators/script.js
@@ -1,5 +1,6 @@
 // Section 1.3 - JavaScript Operators
-
+// For JavaScript Operator Precedence - 
+// https://developer.mozilla.org/my/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
 
 var now = 2018;
 var birthYear = now - 21;
@@ -9,3 +10,14 @@ var birthYear = now - 21 * 2;
 // Total = 1976
 
 console.log(birthYear);
+
+var ageAqeel = 20;
+var ageOni = 21;
+
+ageAqeel = ageOni = (4 + 2) * 5 - 15;
+
+ageAqeel++;
+ageOni *= 2;
+
+console.log(ageAqeel);
+console.log(ageOni);
