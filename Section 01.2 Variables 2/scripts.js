0db123de4d9c7fd6f247a46aa0d00236a5f9aacc
Dibakar <crossbonesdipu@gmail.com>
2018-03-01 03:51:21 +0800

    Prompt, Alert Var

--- Section 01.2 Variables 2/scripts.js
+++ Section 01.2 Variables 2/scripts.js
@@ -5,3 +5,20 @@ var age = 21;
 
 console.log(name + age);
 console.log(age + age);
+
+var job, isMarried;
+
+job = 'Programmer';
+isMarried = false;
+
+console.log('Name: ' + name + ', Age: ' + age + ', Job: ' + job + ', Have Wife: ' + isMarried);
+
+job = 'Driver';
+age = 'Twinty One';
+
+console.log('Name: ' + name + ', Age: ' + age + ', Job: ' + job + ', Have Wife: ' + isMarried);
+
+var lastName = prompt('What is your last name?');
+console.log(lastName);
+
+alert('Congratulations! ' + lastName + ' for viewing my repository');
