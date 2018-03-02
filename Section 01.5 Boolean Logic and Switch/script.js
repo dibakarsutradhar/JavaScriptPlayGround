eab037d2d8cba34e30484bc21bb5b26e933c67ce
Dibakar <crossbonesdipu@gmail.com>
2018-03-02 23:39:32 +0800

    SwitchCase

--- Section 01.5 Boolean Logic and Switch/script.js
+++ Section 01.5 Boolean Logic and Switch/script.js
@@ -7,3 +7,24 @@ if(age <= 19){
 } else {
 	console.log("Man");
 }
+
+//Switch Case
+
+var job = prompt('What does John Do? Choose from teacher/ programmer/ driver/ singer.');
+
+switch(job){
+	case 'teacher':
+		console.log("John is a teacher");
+		break;
+	case 'programmer':
+		console.log("John is a programmer");
+		break;
+	case 'driver':
+		console.log("John is a driver");
+		break;
+	case 'singer':
+		console.log("John is a singer");
+		break;
+	default:
+		console.log("John is an unemloyer");
+}
