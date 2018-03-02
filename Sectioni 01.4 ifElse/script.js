6f52d87058b640a11177a928f4fdef45887cfb19
Dibakar <crossbonesdipu@gmail.com>
2018-03-02 22:37:52 +0800

    Basic ifElse Done

--- Sectioni 01.4 ifElse/script.js
+++ Sectioni 01.4 ifElse/script.js
@@ -2,8 +2,27 @@ var name = 'Afra';
 var age = 22;
 var isMarried = 'No';
 
-if(isMarried === 'Yes'){
-	console.log(name + ' is Married.')
+if(isMarried === 'Yes'){									//With String
+	console.log(name + ' is Married.');
 } else{
-	console.log(name + ' will marry soon.')
-};
+	console.log(name + ' will marry soon.');
+}
+
+//OR
+isMarried = true;
+
+if(isMarried){												//With Boolean
+	console.log('YES Married');
+} else{
+	console.log('NOT Married');
+}
+
+//OR
+if(isMarried) {												//Else dosen't matter much if the statement is TRUE
+	console.log('YES');
+}
+
+//Another Comparision Method
+if(23 == '23'){												//"=="- Used for Coverting variables,"==="- Used for Compairing
+	console.log('Its 23');
+}
