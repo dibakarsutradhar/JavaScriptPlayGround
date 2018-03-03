a676212025755b9f471c796c40e4e9426c3a9bb6
Dibakar <crossbonesdipu@gmail.com>
2018-03-03 22:58:31 +0800

    Retirement Age Function

--- Section 01.7 Functions/script.js
+++ Section 01.7 Functions/script.js
@@ -1,7 +1,19 @@
+// Age Calculator
+
 function calculateAge(yearOfBirth){
 	var age = 2018 - yearOfBirth;
 	return age;
 }
 
 var ageJohn = calculateAge(1997);
-console.log(ageJohn);
+//console.log(ageJohn);
+
+// Retirement Time Calculator
+
+function yearsUntilRetirement(name, year){
+	var age = calculateAge(year);
+	var retirement = 65 - age;
+	console.log (retirement);
+}
+
+yearsUntilRetirement('John', 1990);
