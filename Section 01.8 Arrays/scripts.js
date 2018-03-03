6cf6b143c8d2d9aeabcbf8629b7f8b12ac647a99
Dibakar <crossbonesdipu@gmail.com>
2018-03-03 23:50:25 +0800

    Dot Push

---
+++ Section 01.8 Arrays/scripts.js
@@ -0,0 +1,13 @@
+var names = ['John', 'Mark', 'Hank'];
+var years = new Array(1990, 1991, 1992);
+
+console.log(names[2]);
+console.log(years[1]);
+
+names[1] = 'Ben';
+console.log(names);
+
+var John = ['John', 'Smith', 1992, 'Doctor', false];
+
+John.push('Red');			//	.push - Add arrays
+console.log(John); 
