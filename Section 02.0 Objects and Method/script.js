fe58e8fa06b9162f74bc6af0a606dbbd6b08549e
Dibakar <crossbonesdipu@gmail.com>
2018-03-04 21:29:10 +0800

    2nd Method v2.0

--- Section 02.0 Objects and Method/script.js
+++ Section 02.0 Objects and Method/script.js
@@ -1,3 +1,6 @@
+
+//v1.0
+
 var john = {
 	name: 'John',
 	lastName: 'Smith',
@@ -15,3 +18,24 @@ console.log(john.family);
 console.log(john.calculateAge());
 
 var age = john.calculateAge();
+john.age = age;
+
+console.log(john);
+
+
+//v2.0
+
+var mike = {
+	name: 'Mike',
+	lastName: 'Smith',
+	yearOfBirth: 1970,
+	job: 'Doctor',
+	isMarried: false,
+	family: ['Jane', 'Mark', 'Bob'],
+	calculateAge: function(){
+		this.age = 2018 - this.yearOfBirth;
+	}
+};
+
+mike.calculateAge();
+console.log(mike);
