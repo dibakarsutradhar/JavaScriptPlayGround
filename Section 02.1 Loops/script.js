8f382e8ec91ea21363507314c3582fcdc2199be2
Dibakar <crossbonesdipu@gmail.com>
2018-03-04 22:01:45 +0800

    Break and Continue Statement for Loop

--- Section 02.1 Loops/script.js
+++ Section 02.1 Loops/script.js
@@ -12,7 +12,7 @@ for (var i = 0; i < 5; i++){
 
 // OR
 
-var humanNames = ['John', 'Mark', 'Bob', 'Juan', 'Mata'];
+var humanNames = ['A', 'B', 'C', 'D', 'E'];
 
 for (var i = 0; i < humanNames.length; i++){
 	console.log(humanNames[i]);
@@ -21,8 +21,36 @@ for (var i = 0; i < humanNames.length; i++){
 
 // OR	- From End to Start
 
-var humanNames2 = ['John', 'Mark', 'Bob', 'Juan', 'Mata'];
+var humanNames2 = ['X', 'Y', 'Z', 'r', 'w'];
 
 for (var i = humanNames2.length - 1; i >= 0; i--){
 	console.log(humanNames2[i]);
 }
+
+// WHILE Loop
+
+var i = 0;
+while(i < humanNames2.length){
+	console.log(humanNames2[i]);
+	i++;
+}
+
+// BREAK Statement for Loop
+
+for(i = 1; i >= 0; i++){
+	console.log(i);
+
+	if(i === 3){
+		break;
+	}
+}
+
+// CONTINUE Statement for Loop
+/*
+for(i = 1; i >= 0; i++){
+	if(i === 3){
+		continue;
+	}
+	console.log(i);
+}
+*/
