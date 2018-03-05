64f0f3f51b224056ae43e84c8ffa1b4be8fe3c7f
Dibakar <crossbonesdipu@gmail.com>
2018-03-05 23:59:52 +0800

    Code error fixed

--- Section 02.2 Coding Challenges/script.js
+++ Section 02.2 Coding Challenges/script.js
@@ -1,12 +1,12 @@
-var years = [1998, 1997, 1996, 1995, 1994];
+var years = [1998, 1965, 1956, 1989, 1932];
 var ages = [];
 
-for (var i = 0; i > years.length; i++) {
-	ages[i] = 2016 - years[i];
+for ( var i = 0; i < years.length; i++){
+	ages[i] = 2018 - years[i];
 }
 
-for (i = 0; i < ages.length; i++){
-	if (ages[i] >= 18) {
+for ( i = 0; i < ages.length; i++){
+	if(ages[i] >= 18){
 		console.log('Person is ' + ages[i] + ' years old and is of full age.');
 	} else {
 		console.log('Person is ' + ages[i] + ' years old and is not of full age.');
