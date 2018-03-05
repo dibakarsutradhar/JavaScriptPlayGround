8a3c3092d2cef62d1d164f6145204f9b6cc10104
Dibakar <crossbonesdipu@gmail.com>
2018-03-06 00:15:25 +0800

    Challenge Complete

--- Section 02.2 Coding Challenges/script.js
+++ Section 02.2 Coding Challenges/script.js
@@ -1,14 +1,27 @@
-var years = [1998, 1965, 1956, 1989, 1932];
-var ages = [];
 
-for ( var i = 0; i < years.length; i++){
-	ages[i] = 2018 - years[i];
-}
+function printFullAge(years){
+	var ages = [];
+	var fullAges = [];
 
-for ( i = 0; i < ages.length; i++){
-	if(ages[i] >= 18){
-		console.log('Person is ' + ages[i] + ' years old and is of full age.');
-	} else {
-		console.log('Person is ' + ages[i] + ' years old and is not of full age.');
+
+	for ( var i = 0; i < years.length; i++){
+		ages[i] = 2018 - years[i];
 	}
+
+	for ( i = 0; i < ages.length; i++){
+		if(ages[i] >= 18){
+			console.log('Person ' + (i + 1) + ' is ' + ages[i] + ' years old and is of full age.');
+			fullAges.push(true);
+		} else {
+			console.log('Person ' + (i + 1) + ' is ' + ages[i] + ' years old and is not of full age.');
+			fullAges.push(false);
+		}
+	}
+
+	return fullAges;
 }
+
+var years = [2001, 1965, 1956, 1989, 1932];
+
+var full_1 = printFullAge(years);
+var full_2 = printFullAge([2011, 1995, 1987]);
