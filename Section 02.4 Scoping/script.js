d9bfca406c1ba70124153b57463ca9fd91dd0672
Dibakar <crossbonesdipu@gmail.com>
2018-03-08 22:53:28 +0800

    First Scoping Example

--- Section 02.4 Scoping/script.js
+++ Section 02.4 Scoping/script.js
@@ -1,12 +1,19 @@
-var a = 'Hello';
+
+// First Scoping Example
+
+var a = 'Hello';										// GLOBAL Scope - VoGlobal
 first();
 
-function first(){
+function first(){										// First() Scope - [Vo1] + [VoGlobal] 
 	var b = 'Hi';
 	second();
 
-	function second(){
+	function second(){									// Second() Scope - [Vo2] + [Vo1] + [VoGlobal]
 		var c = 'Hey';
 		console.log(a + b + c);
 	}
 }
+
+
+
+
