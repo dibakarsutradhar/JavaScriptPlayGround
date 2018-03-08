188304f65b25ba4fab5d988b780963b8084cc826
Dibakar <crossbonesdipu@gmail.com>
2018-03-08 23:01:29 +0800

    Executaion Stack and Scope Chain Difference

--- Section 02.4 Scoping/script.js
+++ Section 02.4 Scoping/script.js
@@ -16,4 +16,25 @@ function first(){										// First() Scope - [Vo1] + [VoGlobal]
 
 
 
+// Example to show the difference between executaion stack and scope chain
 
+
+var a = 'Hello';										// GLOBAL Scope - VoGlobal
+first();
+
+function first(){										// First() Scope - [Vo1] + [VoGlobal] 
+	var b = 'Hi';
+	second();
+
+	function second(){									// Second() Scope - [Vo2] + [Vo1] + [VoGlobal]
+		var c = 'Hey';
+		third();
+	}
+}
+
+
+function third(){
+	var d = 'John';
+	//console.log(b + c);								// Third() Can't call second or first function because it's not in Scope Chain
+	console.log(a + d);									// This code will work because variable a is a global scope.
+}
