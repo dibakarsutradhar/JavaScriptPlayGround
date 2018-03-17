50eafc5cc8f140e335aeb1ffcae75a2ae8600a11
Dibakar <crossbonesdipu@gmail.com>
2018-03-17 19:32:17 +0800

    Inheritance with Method and Properties

--- Section 3.0 Creating Objects - Function Constructions/script.js
+++ Section 3.0 Creating Objects - Function Constructions/script.js
@@ -11,9 +11,21 @@ var john = {
 var Person = function (name, yearOfBirth, job) {									// Function Constructor Name's always will start from Capital Letter
 	this.name = name;
 	this.yearOfBirth = yearOfBirth;
-	this.job = job;
+	this.job = job; 
 }
 
+Person.prototype.calculateAge = function(){											// Inheritance with Method
+		console.log(2018 - this.yearOfBirth);
+};
+
+Person.prototype.lastName = 'Smith';												// Inharitance with Properties
 
 var john = new Person ('John', 1980, 'Teacher');
-console.log (john);
+var jane = new Person ('Jane', 1965, 'Doctor');
+var rahim = new Person ('Rahim', 1948, 'Retired');
+
+john.calculateAge();
+jane.calculateAge();
+rahim.calculateAge();
+
+console.log(john.lastName);
