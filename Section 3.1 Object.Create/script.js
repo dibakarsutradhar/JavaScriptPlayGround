7c4c684e9efe994f4ac0ed6c5a6606465bd1eabc
Dibakar <crossbonesdipu@gmail.com>
2018-03-18 18:23:52 +0800

    2nd way of creating object

--- Section 3.1 Object.Create/script.js
+++ Section 3.1 Object.Create/script.js
@@ -6,8 +6,14 @@ var personProto = {
 	}
 };
 
-var john = Object.create (personProto);
+var john = Object.create (personProto);														// One way of creating Object
 
 john.name = 'John';
 john.yearOfBirth = 1990;
 john.job = 'Teacher';
+
+var jane = Object.create (personProto, {													// Another way of Creating Object
+	name: {value: 'Jane'},
+	yearOfBirth: {value: 1965},
+	job: {value: 'Designer'}
+});
