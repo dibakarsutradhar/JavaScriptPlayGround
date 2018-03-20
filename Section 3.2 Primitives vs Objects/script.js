011a1f7cbce6ae1e33c72142c8bc77c20a4c83ab
Dibakar <crossbonesdipu@gmail.com>
2018-03-20 23:47:40 +0800

    functions

--- Section 3.2 Primitives vs Objects/script.js
+++ Section 3.2 Primitives vs Objects/script.js
@@ -1,3 +1,5 @@
+// Primitives
+
 var a = 23;
 var b = a;
 
@@ -6,6 +8,9 @@ a = 46;
 console.log(a);
 console.log(b);
 
+
+
+// Objects
 var obj1 = {
 	name: 'John',
 	age: 26
@@ -15,4 +20,22 @@ var obj2 = obj1;
 obj1.age = 30;
 
 console.log(obj1.age);
-console.log(obj2.age);
+console.log(obj2.age); 
+
+// Functions
+
+var age = 27;
+var obj = {
+	name: 'Dibakar',
+	city: 'Dhaka'
+};
+
+function change(a, b) {
+	a = 30;
+	b.city = 'San Francisco';
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
