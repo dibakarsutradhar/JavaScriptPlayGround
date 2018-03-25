bc76b7bef373f342667bcf38e9a705cf0187c9be
Dibakar <crossbonesdipu@gmail.com>
2018-03-25 23:31:08 +0600

    A Simple function for interview

--- Section 3.4 Functions Returning Functions/script.js
+++ Section 3.4 Functions Returning Functions/script.js
@@ -1,19 +1,26 @@
 // function for Interview Question
 
-function interviewQuestion(job) {
-	if(job === 'designer'){
-		return function(name) {
+function interviewQuestion(job){
+	if(job === 'teacher'){
+		return function (name) {
 			console.log(name + ', Can you please explain what UI Design is?');
-		} else if(job === 'teacher') {
-			return function(name) {
-				console.log(name + ', What do you teach?');
-			}
-		} else {
-			return function(name) {
-				console.log(name + ', What do you do?');
-			}
+		}
+	} else if(job === ' designer') {
+		return function (name) {
+			console.log(name + ', What do you teach?');
+		}
+	} else {
+		return function (name) {
+			console.log(name + ', What do you do?');
 		}
 	}
 }
 
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+
+teacherQuestion('Mark');
+designerQuestion('John');
+
+interviewQuestion('teacher')('Anthony');
 
