c45c4b1df517c76e7ad43a0865f37648448b59e6
Dibakar <crossbonesdipu@gmail.com>
2018-03-26 23:17:28 +0600

    How Closuers Works

--- Section 3.6 Closures/script.js
+++ Section 3.6 Closures/script.js
@@ -1,14 +1,28 @@
 // Retirement Calculator
 
-function retirement (retirementAge) {
+function retirement (retirementAge) {									// GLOBAL SCOPE
 	var a = ' years left for retirement.';
 	return function (yearsOfBirth) {
 		var age = 2018 - yearsOfBirth;
-		console.log(retirementAge - age + a);
+		console.log((retirementAge - age) + a);
 	}
 }
 
-var retirementUS = retirement(66);
-retirementUS(1990);
+var retirementUS = retirement(66);										// GLOBAL EXECUTION CONTEXT
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
 
-retirement(66)(1994);
+retirementGermany(1990);
+retirementUS(1990);
+retirementIceland(1990);
+
+// OR
+
+//retirement(66)(1994);
+
+
+
+
+///// HOW CLOSURES WORKS /////
+
+/* An Inner Function Has Always Access to the Variables and Parameters of its Outer Function, Even After the Outer Function has Returend. */
