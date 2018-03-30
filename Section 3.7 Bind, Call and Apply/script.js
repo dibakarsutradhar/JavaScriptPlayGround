4a6c2e3a60900f11bfa1ad93bf293871adac03a1
Dibakar <crossbonesdipu@gmail.com>
2018-03-30 20:05:10 +0600

    first function

---
+++ Section 3.7 Bind, Call and Apply/script.js
@@ -0,0 +1,16 @@
+// Bind, Call and 
+
+var john = {
+	name: 'John',
+	age: 29,
+	job: 'Teacher',
+	presentation: function(style, timeOfDay) {
+		if (style === 'formal') {
+			console.log('Good ' + timeOfDay + ', Ladis and Gentlement! I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
+		} else if (style === 'casual') {
+			console.log('Hey! What\'s up? I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
+		}
+	}
+}
+
+john.presentation('casual', 'morning');
