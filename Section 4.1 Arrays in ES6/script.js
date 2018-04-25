6fa5cc7955206074930102442f2012513aee158a
Dibakar <crossbonesdipu@gmail.com>
2018-04-25 21:37:07 +0600

    ES5 For Loop

--- Section 4.1 Arrays in ES6/script.js
+++ Section 4.1 Arrays in ES6/script.js
@@ -1,14 +1,25 @@
 const boxes = document.querySelectorAll('.box');
 
 // ES5
-/*
+
 var boxesArr5 = Array.prototype.slice.call(boxes);
 boxesArr5.forEach(function(cur) {
     cur.style.backgroundColor = 'dodgerblue';
 });
-*/
+
 
 
 // ES6
 
 Array.from(boxes).forEach(cur => cur.style.backgroundColor = 'dodgerblue');
+
+
+// ES5
+
+for(var i = 0; i < boxesArr5.length; i++) {
+    if(boxesArr5[i].className === 'box blue') {
+        continue;
+    }
+
+    boxesArr5[i].textContent = 'I changed to blue';
+}
