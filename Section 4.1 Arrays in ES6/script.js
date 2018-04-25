235de6452fb71b56af7eaf13d6c2157ea7e7cc55
Dibakar <crossbonesdipu@gmail.com>
2018-04-25 21:54:59 +0600

    findIndex & find Method of ES6

--- Section 4.1 Arrays in ES6/script.js
+++ Section 4.1 Arrays in ES6/script.js
@@ -33,3 +33,18 @@ for(const cur of boxesArr6) {
     }
     cur.textContent = 'I changed to blue';
 }
+
+
+// ES5
+var ages = [1, 12, 17, 8, 21, 14, 11];
+
+var full = ages.map(function (cur) {
+    return cur >= 18;
+});
+console.log(full);
+console.log(full.indexOf(true));
+console.log(ages[full.indexOf(true)]);
+
+// ES6
+console.log(ages.findIndex(cur => cur >= 18));
+console.log(ages.find(cur => cur >= 18));
