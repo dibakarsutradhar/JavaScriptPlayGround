03adb9a23493660f830d0783d034ec1a995d43bf
Dibakar <crossbonesdipu@gmail.com>
2018-04-26 19:57:05 +0600

    Rest Parameters ES5vsES6

--- Section 4.3 Rest Parameters/script.js
+++ Section 4.3 Rest Parameters/script.js
@@ -1,3 +1,5 @@
+
+/*
 // ES5
 
 function isFullAge5() {
@@ -19,3 +21,23 @@ function isFullAges6(...years) {
 }
 
 isFullAges6(1998, 1889, 1997, 2001);
+*/
+
+// ES5
+function isFullAge5(limit) {
+    var argsArr = Array.prototype.slice.call(arguments, 1);
+    
+    argsArr.forEach(function (cur) {
+        console.log((2018 - cur) >= limit);
+    })
+}
+
+//isFullAge5(21, 1998, 1889, 1997, 2001);
+
+
+// ES6
+function isFullAges6(limit, ...years) {
+    years.forEach(cur => console.log((2018 - cur) >= limit));
+}
+
+isFullAges6(18, 1998, 1889, 1997, 2001);
