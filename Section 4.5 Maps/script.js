7c5883f8e88e644e0dbecfb4a817a4f63d36fbbf
Dibakar <crossbonesdipu@gmail.com>
2018-04-27 23:27:19 +0600

    MAP quiz finishes. Reason of Using Map over Object.

--- Section 4.5 Maps/script.js
+++ Section 4.5 Maps/script.js
@@ -12,17 +12,33 @@ question.set(true, 'Correct Answer!');
 question.set(false, 'Wrong Answer, Try Again!');
 
 console.log(question.get('question'));
-console.log(question.size);
+//console.log(question.size);
 
 if(question.has(4)) {
     //question.delete(4);
-    console.log('Testing Has');
+    //console.log('Testing Has');
 }
 
 // question.clear();
 
-question.forEach((value, key) => console.log(`This is ${key} and it's set to ${value}`));
+//question.forEach((value, key) => console.log(`This is ${key} and it's set to ${value}`));
 
 for(let [key, value] of question.entries()) {
-    console.log(`This is ${key} and it's set to ${value}`);
+    if(typeof(key) === 'number'){
+        console.log(`Answer ${key} : ${value}`);
+    }
 }
+
+const ans = parseInt(prompt('Write the correct answer!'));
+
+console.log(question.get(ans === question.get('correct')));
+
+
+/*
+    WHY MAPS ARE BETTER THEN OBJECTS?
+
+        1. We can use anything as KEY
+        2. MAPS are iterable, which make it easy to Loop through them and manipulate data with them.
+        3. It's very easy to get the size of MAP using the .size property.
+        4. We can easily add and remove data from MAP.
+*/
