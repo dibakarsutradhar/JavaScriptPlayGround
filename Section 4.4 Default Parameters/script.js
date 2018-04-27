31047910c0676e758a435600caf0deda4647bd02
Dibakar <crossbonesdipu@gmail.com>
2018-04-27 22:08:36 +0600

    ES6 Version of Default Parameters

--- Section 4.4 Default Parameters/script.js
+++ Section 4.4 Default Parameters/script.js
@@ -14,3 +14,14 @@ function SmithPerson(firstName, yearOfBirth, lastName, nationality) {
 
 var john = new SmithPerson('John', 1990);
 var max = new SmithPerson('Max', 1988, 'Hardy', 'Latino');
+
+// ES6
+function JokerPerson(firstName, yearOfBirth, lastName = 'Joker', nationality = 'American') {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.yearOfBirth = yearOfBirth;
+    this.nationality = nationality;
+}
+
+var batman = new JokerPerson('Batman', 1965);
+var superman = new JokerPerson('Super', 1975, 'Man', 'Kryptonian');
