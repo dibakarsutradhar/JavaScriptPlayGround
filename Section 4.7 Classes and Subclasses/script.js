754575273669f3c73da5f3c32370412db1f529ab
Dibakar <crossbonesdipu@gmail.com>
2018-04-28 22:25:24 +0600

    Connecting 2 funct constructor in ES5

---
+++ Section 4.7 Classes and Subclasses/script.js
@@ -0,0 +1,32 @@
+// Classes and Subclasses
+
+// ES5
+
+var Person5 = function (name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person5.prototype.calculateAge = function () {
+    var age = new Date().getFullYear() - this.yearOfBirth;
+    console.log(age);
+}
+
+var Athlete5 = function (name, yearOfBirth, job, olympicGames, medals) {
+    Person5.call(this, name, yearOfBirth, job);
+    this.olympicGames = olympicGames;
+    this.medals = medals;
+}
+
+Athlete5.prototype = Object.create(Person5.prototype);
+
+Athlete5.prototype.wonMedal = function() {
+    this.medals++;
+    console.log(this.medals);
+}
+
+var john5 = new Athlete5 ('John', 1990, 'Cyclist', 4, 11);
+
+john5.calculateAge();
+john5.wonMedal();
