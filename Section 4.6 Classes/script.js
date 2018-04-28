63bcfdd4dcd1936cbc6ffb0f0a5e28a9ad01ba60
Dibakar <crossbonesdipu@gmail.com>
2018-04-28 21:40:14 +0600

    Classes with ES5

---
+++ Section 4.6 Classes/script.js
@@ -0,0 +1,16 @@
+// Classes
+
+// ES5
+
+var Person5 = function (name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person5.prototype.calculateAge = function () {
+    var age = new Date().getFullYear - this.yearOfBirth;
+    console.log(age);
+}
+
+var john5 = new Person5('John', 1990, 'Teacher');
