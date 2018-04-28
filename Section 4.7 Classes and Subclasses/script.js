d1b913574a11a8af0e4514da8eb6d3bb65231c77
Dibakar <crossbonesdipu@gmail.com>
2018-04-29 00:06:18 +0600

    Connecting 2 funct constructor in ES6

--- Section 4.7 Classes and Subclasses/script.js
+++ Section 4.7 Classes and Subclasses/script.js
@@ -30,3 +30,37 @@ var john5 = new Athlete5 ('John', 1990, 'Cyclist', 4, 11);
 
 john5.calculateAge();
 john5.wonMedal();
+
+
+// ES6
+
+class Person6 {
+    constructor(name, yearOfBirth, job) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.job = job;
+    }
+
+    calculateAge() {
+        var age = new Date().getFullYear() - this.yearOfBirth;
+        console.log(age);
+    }
+}
+
+class Athlete6 extends Person6 {
+    constructor(name, yearOfBirth, job, olympicGames, medals) {
+        super(name, yearOfBirth, job);
+        this.olympicGames = olympicGames;
+        this.medals = medals;
+    }
+
+    wonMedal() {
+        this.medals++;
+        console.log(this.medals);
+    }
+}
+
+const john6 = new Athlete6('John', 1990, 'Cyclist', 4, 11);
+
+john6.wonMedal();
+john6.calculateAge();
