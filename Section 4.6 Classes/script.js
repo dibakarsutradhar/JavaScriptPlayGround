a07bb3139bec6952c45ec30c0a2477ba443355d9
Dibakar <crossbonesdipu@gmail.com>
2018-04-28 21:47:55 +0600

    ES6 Class and Constructor

--- Section 4.6 Classes/script.js
+++ Section 4.6 Classes/script.js
@@ -14,3 +14,21 @@ Person5.prototype.calculateAge = function () {
 }
 
 var john5 = new Person5('John', 1990, 'Teacher');
+
+
+// ES6
+
+class Person6 {
+    constructor (name, yearOfBirth, job) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.job = job;
+    }
+
+    calculateAge () {
+        var age = new Date().getFullYear - this.yearOfBirth;
+        console.log(age);
+    }
+}
+
+const john6 = new Person6('John', 1990, 'Teacher');
