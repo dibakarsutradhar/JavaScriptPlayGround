3f40afbcc625fdef4e766a631f993b01e735cd16
Dibakar <crossbonesdipu@gmail.com>
2018-05-08 22:35:59 +0600

    Two Elements Created

--- Section 4.8 Coding Challenges/script.js
+++ Section 4.8 Coding Challenges/script.js
@@ -10,8 +10,8 @@ Suppose that you're working in a small town administration, and you're in charge
 It's a very small town, so right now there are only 3 parks and 4 streets. All parks and streets have a name and a build year.
 
 At an end-of-year meeting, your boss wants a final report with the following:
-1. Tree density of each park in the town (forumla: number of trees/park area)
-2. Average age of each town's park (forumla: sum of all ages/number of parks)
+1. Tree density of each park in the town (formula: number of trees/park area)
+2. Average age of each town's park (formula: sum of all ages/number of parks)
 3. The name of the park that has more than 1000 trees
 4. Total and average length of the town's streets
 5. Size classification of all streets: tiny/small/normal/big/huge. If the size is unknown, the default is normal
@@ -21,3 +21,43 @@ All the report data should be printed to the console.
 HINT: Use some of the ES6 features: classes, subclasses, template strings, default parameters, maps, arrow functions, destructuring, etc.
 
 */
+
+class Element {
+    constructor(name, buildYear) {
+        this.name = name;
+        this.buildYear = buildYear;
+    }
+}
+
+class Park extends Element {
+    constructor(name, buildYear, area, numTrees) {
+        super(name, buildYear);
+        this.area = area;       // Unit = Km2
+        this.numTrees = numTrees;
+    }
+
+    densityTree() {
+        const density = this.numTrees / this.area;
+
+        console.log(`${this.name} has a tree density of ${density} trees per square km.`);
+    }
+}
+
+class Street extends Element {
+    constructor(name, buildYear, length, size = 3) {
+        super(name, buildYear);
+        this.length = length;
+        this.size = size;
+    }
+
+    classifyStreet() {
+        const classification = new Map();
+        classification.set(1, 'tiny');
+        classification.set(2, 'small');
+        classification.set(3, 'normal');
+        classification.set(4, 'big');
+        classification.set(5, 'huge');
+        
+        console.log(`${this.name}, build in ${this.buildYear}, is a ${classification.get(this.size)} street.`);
+    }
+}
