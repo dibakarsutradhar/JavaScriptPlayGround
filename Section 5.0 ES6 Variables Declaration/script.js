7054497ce8b8b777eb7adf82ff95ed7f056ec277
Dibakar <crossbonesdipu@gmail.com>
2018-05-12 21:43:33 +0600

    Var, Let, Const

--- Section 5.0 ES6 Variables Declaration/script.js
+++ Section 5.0 ES6 Variables Declaration/script.js
@@ -1,5 +1,6 @@
 // Let and Const
 
+/*
 // ES5
 var name5 = 'Jon Doe';
 var age5 = 23;
@@ -11,3 +12,30 @@ const name6 = 'Jon Doe';            // Constant Variable - Can't Change
 let age6 = 23;                      // It can be change
 name6 = 'Jon Donald';               // Won't Work - Because, name6 is a const variable
 console.log(name6);
+*/
+
+// ES5
+function drivingLicense5 (passedTest) {
+    if (passedTest) {
+        console.log(firstName);
+        var firstName = 'John';                                             // var is a function scop
+        var yearOfBirth = 1990;
+    }
+    console.log(firstName + ', Born in ' + yearOfBirth + ', is now officially allowed to drive a car.');     // so it will work   
+}
+
+drivingLicense5(true);
+
+// ES6
+function drivingLicense6(passedTest) {
+    // console.log(firstName);
+    let firstName;
+    const yearOfBirth = 1990;
+
+    if (passedTest) {
+        firstName = 'John';                                             // let and const are block scop, outside of {} block they will not work
+    }
+    console.log(firstName + ', Born in ' + yearOfBirth + ', is now officially allowed to drive a car.');       // so it won't work
+}
+
+drivingLicense6(true);
