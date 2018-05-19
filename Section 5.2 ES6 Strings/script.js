6326ceb475e263b74f057853700312e2c9d24229
Dibakar <crossbonesdipu@gmail.com>
2018-05-19 19:15:39 +0600

    ES6 startsWtih, endsWith, Includes, repeat method

--- Section 5.2 ES6 Strings/script.js
+++ Section 5.2 ES6 Strings/script.js
@@ -13,3 +13,11 @@ console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + year
 
 // ES6
 console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today, he is ${calcAge(yearOfBirth)} years old.`);
+
+
+const n = `${firstName} ${lastName}`;
+console.log(n.startsWith('J'));                     // Tastes if a string starts with something
+console.log(n.endsWith('mi'));                      // Tastes if a string ends with something
+console.log(n.includes(' '));                       // Tastes if a string includes something
+console.log(firstName.repeat(5));
+console.log(`${firstName} `.repeat(5));
