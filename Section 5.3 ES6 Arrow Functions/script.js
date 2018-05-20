369855ef3be3fdc63106e96ca6e121161cf571c6
Dibakar <crossbonesdipu@gmail.com>
2018-05-20 21:07:20 +0600

    3 Ways of Using Arrow Function

---
+++ Section 5.3 ES6 Arrow Functions/script.js
@@ -0,0 +1,25 @@
+const years = [1990, 1989, 1987, 1997, 2000];
+
+// ES5
+var ages5 = years.map(function (el) {
+    return 2018 - el;
+});
+
+console.log(ages5);
+
+
+// ES6
+let ages6 = years.map(el => 2018 - el);
+
+console.log(ages6);
+
+ages6 = years.map((el, index) => `Age element ${index + 1} : ${2018 - el}.`);
+console.log(ages6);
+
+ages6 = years.map((el, index) => {
+    const now = new Date().getFullYear();
+    const age = now - el;
+    return `Age element ${index + 1} : ${age}.`;
+});
+
+console.log(ages6);
